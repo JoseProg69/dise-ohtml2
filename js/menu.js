c7b28e791ffb343206ef42592d6115992f9fc472
jose <josegandia2003@gmail.com>
2025-02-28 13:07:26 +0100

    chore: commit inicial

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+const btn =document.getElementById("hamburguesa");
+const menu =document.querySelector("header nav");
+
+btn.addEventListener("click", (event:) =>){
+    btn.classList.toggle("activo");
+}
+
+
+
